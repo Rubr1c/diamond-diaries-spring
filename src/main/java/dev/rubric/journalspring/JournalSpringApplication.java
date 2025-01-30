f482846eab9b72bf8378c4a078a8a44abbec3b65
Rubr1c <alizaghloul64@gmail.com>
2025-01-30 21:48:28 +0200

    init commit

---
+++ src/main/java/dev/rubric/journalspring/JournalSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.rubric.journalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rubric/journalspring/JournalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rubric.journalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
