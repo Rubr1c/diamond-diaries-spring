e182485cf22c401d4bfe9a223b96e86182d3e048
Rubr1c <alizaghloul64@gmail.com>
2025-02-27 16:58:35 +0200

    Remove isFavorite field from EntryDto

--- src/main/java/dev/rubric/journalspring/dto/EntryDto.java
+++ src/main/java/dev/rubric/journalspring/dto/EntryDto.java
@@ -5,4 +5,4 @@ import dev.rubric.journalspring.models.Tag;
 
 import java.util.Set;
 
-public record EntryDto(String title, Folder folder, String content, Set<Tag> tags, Integer wordCount, boolean isFavorite) { }
+public record EntryDto(String title, Folder folder, String content, Set<Tag> tags, Integer wordCount) { }
