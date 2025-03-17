64441c240637b4f5101f80beb50e22ad219bb99e
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2025-03-17 14:52:38 +0200

    Add S3Service, relevant env variables and MediaRepository

---
+++ src/main/java/dev/rubric/journalspring/repository/MediaRepository.java
@@ -0,0 +1,7 @@
+package dev.rubric.journalspring.repository;
+
+import dev.rubric.journalspring.models.Media;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MediaRepository extends CrudRepository<Media, Long> {
+}
---
+++ src/main/java/dev/rubric/journalspring/service/MediaService.java
@@ -0,0 +1,8 @@
+package dev.rubric.journalspring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MediaService {
+
+}
