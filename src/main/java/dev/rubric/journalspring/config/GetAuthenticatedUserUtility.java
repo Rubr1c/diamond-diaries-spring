325ab92963b4e9387456e8f51c324b68fb579a2d
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2025-03-22 21:34:10 +0200

    Add get authenticated user utility

---
+++ src/main/java/dev/rubric/journalspring/config/GetAuthenticatedUserUtility.java
@@ -0,0 +1,28 @@
+package dev.rubric.journalspring.config;
+
+import dev.rubric.journalspring.models.User;
+import dev.rubric.journalspring.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetAuthenticatedUserUtility {
+
+    private final UserService userService;
+
+    public GetAuthenticatedUserUtility(UserService userService) {
+        this.userService = userService;
+    }
+
+    public User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new SecurityException("User is not authenticated");
+        }
+
+        String username = authentication.getName();
+        return userService.findByUsername(username);
+    }
+}
